ec845b32eee803abf889987ac43643878cdc1c31
Abishek <abishek0398@gmail.com>
2019-06-22 17:57:10 +0530

    removed syntax_errors

--- memory.v
+++ memory.v
@@ -1,13 +1,16 @@
 module memory#(parameter WORD_LENGTH = 32,
-			 MEMORY_SIZE = 32;
+			 MEMORY_SIZE = 32
 	      )
              ( input [MEMORY_SIZE-1:0] address,
 	       input [MEMORY_SIZE-1:0] write_add,
 	       input [WORD_LENGTH-1:0] write_data,
 	       input write_enable,
 	       input read_enable,
+               input clk,
+               input rst,
 	       output [WORD_LENGTH-1:0] data_out
 	      );
+integer i;
 reg [WORD_LENGTH-1:0] word_line[MEMORY_SIZE-1:0];
 always @(posedge(clk) or negedge(rst))
 begin
@@ -24,3 +27,4 @@ begin
 	end
 end
 assign data_out = (read_enable) ? word_line[address]:0;
+endmodule
