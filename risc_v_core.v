c0b8f90b85dfbbbe3edf3fbd16a9690d19e89491
Abishek <abishek0398@gmail.com>
2019-06-23 17:45:02 +0530

    rectified port connections

--- risc_v_core.v
+++ risc_v_core.v
@@ -21,9 +21,9 @@ module risc_v #(parameter WORD_LENGTH =32)
 
 	       wire [WORD_LENGTH-1 :0] reg_op1,reg_op2; // outputs of registerfile
 
-               memory instruction_mem (.address(pc),.write_add(0),.write_data(0),.write_enable(1'b 0),.read_enable(1'b 1),.data_out(instr)); // instantiating instruction memory 
+               memory instruction_mem (.address(pc),.write_add(0),.write_data(0),.write_enable(1'b 0),.read_enable(1'b 1),.clk(clk),.rst(reset),.data_out(instr)); // instantiating instruction memory 
 
-               register_file r_file (.add_rs1(instr[19:15]),.add_rs2(instr [24:20]),.write_add(instr [11:7]),.write_data(alu_out),.write_enable(reg_write_en),.clk(clk),.rst(1'b 1),.data_1(reg_op1),.data_2(reg_op2));// r_file 
+               register_file r_file (.add_rs1(instr[19:15]),.add_rs2(instr [24:20]),.write_add(instr [11:7]),.write_data(alu_out),.write_enable(reg_write_en),.clk(clk),.rst(reset),.data_1(reg_op1),.data_2(reg_op2));// r_file 
 
                alu risc_alu (.in_1(reg_op1),.in_2(alu_ip2),.cin(sub_signal),.op_mux_ctrl(alu_ctrl),.alu_out(alu_out),.carry_out(carry_out),.zero(zero),.sign(sign)); // alu instantiation
 
